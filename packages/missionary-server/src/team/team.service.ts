6c88adced3cca257b4dba6e1471155084a08db86
juchanhwang <ghkdwncks96@gmail.com>
2026-02-07 17:34:45 +0900

    style(server): fix prettier formatting in team.service.ts

--- packages/missionary-server/src/team/team.service.ts
+++ packages/missionary-server/src/team/team.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
-
 import { PrismaService } from '@/database/prisma.service';
 
 import { AddMembersDto } from './dto/add-members.dto';
